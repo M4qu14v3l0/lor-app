38d29e94523564afc3bb4dcd4b324af6d0c1eb8f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-02-06 13:03:43 -0500

    fixed model node type

--- src/app/(home)/special/ui/Model.tsx
+++ src/app/(home)/special/ui/Model.tsx
@@ -3,12 +3,14 @@ import { useGLTF } from "@react-three/drei"
 export const Model = () => {
   const { nodes , materials} = useGLTF('/Gollum3D/Gollum.glb')
 
+  const outputMesh = nodes.output as any;
+
   return (
     <group>
     <mesh
       castShadow
       receiveShadow
-      geometry={nodes.output.geometry}
+      geometry={outputMesh.geometry}  
       material={materials.material_1}
       position={[0, 1.489, 0]}
       rotation={[Math.PI / 2, 0, 1.258]}
