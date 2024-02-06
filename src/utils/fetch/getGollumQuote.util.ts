41e3776ead920edb5f89989d4e5b0e808ea3a366
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-02-06 15:43:18 -0500

    getGollumQuotes reworked

--- src/utils/fetch/getGollumQuote.util.ts
+++ src/utils/fetch/getGollumQuote.util.ts
@@ -1,21 +1,20 @@
 
 export const getGollumQuote = async (id: string) => {
 
-  try{ 
-    const response = await fetch( `https://the-one-api.dev/v2/quote/${id}`, {
+  try {
+    const response = await fetch(`https://the-one-api.dev/v2/quote/${id}`, {
       cache: 'force-cache',
       method: 'get',
       headers: {
         Authorization: `Bearer ${process.env.API_KEY}`,
         'Content-Type': 'application/json'
       }
-    })
-    
+    });
+
     const data = await response.json();
-    return data.docs[0];
-  
-  } catch(error) {
-    console.error(`Error getting gollum quote: https://the-one-api.dev/v2/quote/${id}`, error );
+    return data.docs && data.docs.length > 0 ? data.docs[0] : null;
+  } catch (error) {
+    console.error(`Error getting gollum quote: https://the-one-api.dev/v2/quote/${id}`, error);
     throw error;
   }
 }
